fb1e23587cc27f54ed18a3fffdf899f5ed092914
kirargomedo23 <kirargomedo@gmail.com>
2021-11-28 19:21:26 -0500

    listado de productos finish

--- app/src/main/java/com/example/proyectofinal/ENTIDADES/Producto.java
+++ app/src/main/java/com/example/proyectofinal/ENTIDADES/Producto.java
@@ -1,5 +1,7 @@
 package com.example.proyectofinal.ENTIDADES;
 
+import java.util.List;
+
 public class Producto {
 
     private Integer idProducto;
@@ -9,7 +11,64 @@ public class Producto {
     private Integer cantidad;
     private Integer stock;
 
+    private Categoria categoria;
 
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Float precio) {
+        this.precio = precio;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 
 
 
--- app/src/main/java/com/example/proyectofinal/ListadoProducto.java
+++ app/src/main/java/com/example/proyectofinal/ListadoProducto.java
@@ -5,13 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ListadoProducto extends AppCompatActivity {
+
+    private String productos [] = {"Gaseosa","fewef","Dante","Castillo","José", "Samuel", "Diego"};
+    //private String edades [] = {"12","22","55","23","33","78","64"};
+
+    private TextView tv1;
+    private ListView lv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado_producto);
+
+        tv1 = (TextView)findViewById(R.id.tv1);
+        lv1 = (ListView)findViewById(R.id.lv1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item_geekipedia, productos);
+        lv1.setAdapter(adapter);
+
+        /*lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                tv1.setText("La edad de: "+lv1.getItemAtPosition(i)+" es : "+edades[i]+ " años.");
+            }
+        });*/
+
     }
 
 
